bbc7fe29786f04bc33bc16da6f7b4a07efc8d4ba
Bruno <bruno.a@outlook.com.br>
2021-10-16 15:05:56 -0300

    adding logic

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,4 +2,11 @@ import react from "react";
 import './Button.css';
 
 export default props =>
-    <button className="button">{props.label}</button>
+    <button className={`
+        button
+        ${props.operation ? 'operation': ''}
+        ${props.double ? 'double' : ''}
+        ${props.tiple ? 'tiple' : ''}
+    `} onClick={e => props.click && props.click(props.label)}>
+        {props.label}
+    </button>
